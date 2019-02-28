cb4096e5d915c7b434bc1fafbf34e870243f052b
Andrew Slejzer <brokensound548@gmail.com>
2019-02-28 17:25:20 -0500

    Inital commit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# hello_world.py
+
+# Created by Andrew Slejzer on  2/28/19
+
+# email 00266097@student.necc.edu
+# ------------------------------------------------------------------ #
+
+# print hello world to console
+print("hello world")
